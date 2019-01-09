22ff1fa4d611dff079e3468e601ff4490fa540fd
masterechez <emekaechezona@icloud.com>
2019-01-09 12:33:06 -0500

    Update researchwing.py

--- LowTechGame/researchwing.py
+++ LowTechGame/researchwing.py
@@ -11,7 +11,7 @@ def cim():
 
 dennis_unleashed = False
 
-def resWing():
+def research():
     """
     Player actions while in the research wing
     """
@@ -43,7 +43,30 @@ def resWing():
             choice = raw_input("Do you want to pick it up? (Y/N)").lower()
             sleep(0.75)
             
-            while choice not in "pc":
+            while choice not in "yn":
                 print("That wasn't a choice. Choose Y to pick it up",end="")
                 choice = raw_input("or N if you don't want to. ")
                 sleep(0.75)
+
+            if choice == "y":
+                inventoryAdd('flashlight')
+            else:
+                print("You decide not to pick up the flashlight.")
+                sleep(0.75)
+        else:
+            choice = raw_input("What computer cart (A-E) do you go to? ").lower()
+            sleep(0.75)
+            
+            while choice not in "abcde":
+                choice = raw_input("Choose a computer cart (a, b, c, d, or e) to go to: ")
+                sleep(0.75)
+            
+            print("You head to cart",choice.upper(),"but you see that there is nothing to")
+            sleep(0.25)
+            print("sign out a laptop (you wouldn't dare take one without")
+            sleep(0.25)
+            print("signing it out), so you leave it.")
+            sleep(0.75)
+        
+        print("You leave the research wing and head back to the hallways.")
+        sleep(1)
